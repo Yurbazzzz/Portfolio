38419355d6cf1fe6eac348c054b0cf35a831215d
Yurbazzzz <74986111+Yurbazzzz@users.noreply.github.com>
2021-11-18 04:17:40 +0200

    Day 4

--- Localli/js/switch-theme.js
+++ Localli/js/switch-theme.js
@@ -1,4 +1,33 @@
 function switchTheme (){
     let elem = document.querySelector('#body');
         elem.classList.toggle('black-theme');
+
+    elem = document.querySelector('#telega');
+    elem.classList.toggle('telega-black');
+
+    elem = document.querySelector('#cardText');
+    elem.classList.toggle('text-black');
+
+    elem = document.querySelector('#btn');
+    elem.classList.toggle('button-black');
+
+    elem = document.querySelector('#btn2');
+    elem.classList.toggle('button-black');
+
+    elem = document.querySelector('#btn3');
+    elem.classList.toggle('button-black');
+
+    elem = document.querySelector('#btn4');
+    elem.classList.toggle('button-black');
+
+    elem = document.querySelector('#btn5');
+    elem.classList.toggle('button-black');
+
+    elem = document.querySelector('#btn6');
+    elem.classList.toggle('button-black');
+
+    elem = document.querySelector('#btn7');
+    elem.classList.toggle('button-black');
+
+
 }
